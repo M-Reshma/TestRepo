8979c11534d2322b638a2e39e127a92c67020f0d
SHAIK SADDAM HUSSAIN <samshaiksaddam@gmail.com>
2020-04-15 21:23:42 +0530

    Initial Commit

---
+++ TestRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRepo
+//
+//  Created by kartheek.p on 15/04/20.
+//  Copyright © 2020 Reshma.M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestRepoTests/TestRepoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestRepoTests.swift
+//  TestRepoTests
+//
+//  Created by kartheek.p on 15/04/20.
+//  Copyright © 2020 Reshma.M. All rights reserved.
+//
+
+import XCTest
+@testable import TestRepo
+
+class TestRepoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
